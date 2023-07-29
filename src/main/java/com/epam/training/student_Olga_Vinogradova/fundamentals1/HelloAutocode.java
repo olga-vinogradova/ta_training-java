3214d15fd011a8d7090a1e40f4104820bbc47f67
Olga Vinogradova <olga.hohlova301189@gmail.com>
2023-07-29 22:19:37 +0300

    add 2 tasks

--- src/main/java/com/epam/training/student_Olga_Vinogradova/fundamentals1/HelloAutocode.java
+++ src/main/java/com/epam/training/student_Olga_Vinogradova/fundamentals1/HelloAutocode.java
@@ -1,4 +1,7 @@
 package com.epam.training.student_Olga_Vinogradova.fundamentals1;
 
-public class TaskMeetAutocode {
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+    }
 }
--- src/main/java/com/epam/training/student_Olga_Vinogradova/fundamentals1/MeetAStranger.java
+++ src/main/java/com/epam/training/student_Olga_Vinogradova/fundamentals1/MeetAStranger.java
@@ -1,2 +1,11 @@
-package com.epam.training.student_Olga_Vinogradova.fundamentals1;public class MeetAStranger {
+package com.epam.training.student_Olga_Vinogradova.fundamentals1;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Hello, " + input);
+    }
 }
